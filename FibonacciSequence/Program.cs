0a882b5606a769d6dae788512824586cd9c2fdd4
Gita <gitavejdani@gmail.com>
2025-11-05 15:59:35 +0330

    add fibonacci project

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FibonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < +10; i++)
+                Console.WriteLine(Fibonacci(i) + "");
+        }
+            public static int Fibonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+    
+}
